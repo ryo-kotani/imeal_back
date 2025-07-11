97813ce75e32e298d8e187acd1d58d6885a3c8fc
ryo-kotani <r.kotani@infomart.co.jp>
2025-07-11 14:34:49 +0900

    Initial commit

---
+++ src/main/java/com/imeal/imeal_back/ImealBackApplication.java
@@ -0,0 +1,13 @@
+package com.imeal.imeal_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImealBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImealBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imeal/imeal_back/ImealBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imeal.imeal_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImealBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
