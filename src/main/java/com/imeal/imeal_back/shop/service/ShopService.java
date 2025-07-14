09dcad6ee127fcff267c4a1505ca00e907841d22
sima1126 <s.ogawa@infomart.co.jp>
2025-07-14 17:44:12 +0900

    shop一覧を取得
    findbyXメソッドからshopの一覧を取得する

--- src/main/java/com/imeal/imeal_back/shop/service/ShopService.java
+++ src/main/java/com/imeal/imeal_back/shop/service/ShopService.java
@@ -1,8 +1,23 @@
 package com.imeal.imeal_back.shop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.imeal.imeal_back.shop.dto.ShopListResponse;
+import com.imeal.imeal_back.shop.entity.Shop;
+import com.imeal.imeal_back.shop.repository.ShopRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ShopService {
-  
+  private final ShopRepository shopRepository;
+
+  public ShopListResponse getShops(Integer baseId) {
+    List<Shop> shops = shopRepository.findByX(baseId);
+    return new ShopListResponse(shops);
+  }
 }
