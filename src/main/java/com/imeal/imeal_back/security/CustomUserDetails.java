4a902216315fdad62893ec097d2083d80787f146
ryo-kotani <r.kotani@infomart.co.jp>
2025-07-14 17:31:07 +0900

    17-7login, logout

--- src/main/java/com/imeal/imeal_back/security/CustomUserDetails.java
+++ src/main/java/com/imeal/imeal_back/security/CustomUserDetails.java
@@ -1,5 +1,63 @@
 package com.imeal.imeal_back.security;
 
-public class CustomUserDetails {
-  
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.imeal.imeal_back.user.entity.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomUserDetails implements UserDetails {
+
+  private final User user;
+
+  public CustomUserDetails(User user) {
+    this.user = user;
+  }
+
+  // レスポンス用
+  public User getUser() {
+    return user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
--- src/main/java/com/imeal/imeal_back/security/SecurityConfig.java
+++ src/main/java/com/imeal/imeal_back/security/SecurityConfig.java
@@ -1,8 +1,113 @@
 package com.imeal.imeal_back.security;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imeal.imeal_back.common.config.CorsProperties;
+import com.imeal.imeal_back.user.dto.UserResponse;
+import com.imeal.imeal_back.user.service.UserMapper;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
   
+  private final UserMapper userMapper;
+  private final ObjectMapper objectMapper;
+  private final CorsProperties corsProperties;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(cors -> cors
+        .configurationSource(request -> {
+          var corsConfiguration = new CorsConfiguration();
+          corsConfiguration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+          corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+          corsConfiguration.setAllowCredentials(true);
+          corsConfiguration.setAllowedHeaders(List.of("*"));
+          return corsConfiguration;
+        }))
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+          // ここに記述したパスへのPOSTメソッドはログインなしで許可
+          .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+          .anyRequest().authenticated())
+        .formLogin(login -> login
+          .loginProcessingUrl("/api/login")
+          .usernameParameter("email")
+          .successHandler(authenticationSuccessHandler())
+          .failureHandler(authenticationFailureHandler())
+          .permitAll())
+        .logout(logout -> logout
+          .logoutUrl("/api/logout")
+          .logoutSuccessHandler(logoutSuccessHandler()));
+    return httpSecurity.build();
+  }
+
+  // passwordのデフォルトエンコードアルゴリズム
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  // login成功時の処理
+  @Bean
+  public AuthenticationSuccessHandler authenticationSuccessHandler() {
+    return (request, response, authentication) -> {
+      CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+      UserResponse responseBody = userMapper.toResponse(userDetails.getUser());
+
+      response.setStatus(HttpServletResponse.SC_OK);
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      // objectMapperでjsonに変換
+      response.getWriter().write(objectMapper.writeValueAsString(responseBody));
+    };
+  }
+
+  // login失敗時の処理
+  @Bean
+  public AuthenticationFailureHandler authenticationFailureHandler() {
+    return (request, response, exception) -> {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write(objectMapper.writeValueAsString((
+        Map.of("errorMessages", List.of("ログインに失敗しました"))
+      )));
+    };
+  }
+
+  // logout時の処理
+  @Bean
+  public LogoutSuccessHandler logoutSuccessHandler() {
+    return (request, response, authentication) -> {
+      response.setStatus(HttpServletResponse.SC_OK);
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write(objectMapper.writeValueAsString(
+        Map.of("successMessages", List.of("ログアウトに成功しました"))
+      ));
+    };
+  }
 }
