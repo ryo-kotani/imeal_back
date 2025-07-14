9c5cd36998274d4e3c1eaf93255c2c70c71b63aa
sima1126 <s.ogawa@infomart.co.jp>
2025-07-14 17:44:12 +0900

    shop一覧を取得
    findbyXメソッドからshopの一覧を取得する

--- src/main/java/com/imeal/imeal_back/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/imeal/imeal_back/common/exception/GlobalExceptionHandler.java
@@ -23,7 +23,6 @@ public class GlobalExceptionHandler {
   }
 
   // その他のエラーはここで処理される（各種コントローラーに記載不要）
-  //bbbb
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public Map<String, Object> handleAllUncaughtException(Exception exception) {
--- src/main/java/com/imeal/imeal_back/shop/repository/ShopRepository.java
+++ src/main/java/com/imeal/imeal_back/shop/repository/ShopRepository.java
@@ -3,14 +3,19 @@ package com.imeal.imeal_back.shop.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import com.imeal.imeal_back.shop.entity.Shop;
 
 @Mapper
 public interface ShopRepository {
-  List<Shop> findByX(String locationIdQuery);
+  @SelectProvider(type = ShopSqlBuilder.class, method = "buildSearchSql")
+  List<Shop> findByX(@Param("baseId")Integer baseId);
   Shop findById(Integer id);
   void insert(Shop shop);
   void update(Shop shop);
   void delete(Integer id);
+  // locationIdQuery = "where base_id = 1"
+  // locationIdQuery = "where base_id = " + locationId
 }
