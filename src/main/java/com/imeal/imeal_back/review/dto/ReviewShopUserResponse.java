728fbe112811ff0dea8c036cb7f61a8576965d26
ryo-kotani <r.kotani@infomart.co.jp>
2025-07-16 17:30:57 +0900

    ReviewShopUserResponseクラスを作成

---
+++ src/main/java/com/imeal/imeal_back/review/dto/ReviewShopUserResponse.java
@@ -0,0 +1,12 @@
+package com.imeal.imeal_back.review.dto;
+
+import com.imeal.imeal_back.review.entity.Review;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewShopUserResponse {
+  private Review review;
+}
