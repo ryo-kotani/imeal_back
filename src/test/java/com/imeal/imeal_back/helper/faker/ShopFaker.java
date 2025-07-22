cc4a866c47a2255dadecd81bce7196e3a47523da
ryo-kotani <r.kotani@infomart.co.jp>
2025-07-22 11:46:33 +0900

    shopTestDataFactory作成

---
+++ src/test/java/com/imeal/imeal_back/helper/faker/ShopFaker.java
@@ -0,0 +1,34 @@
+package com.imeal.imeal_back.helper.faker;
+
+import org.springframework.stereotype.Component;
+
+import com.github.javafaker.Faker;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ShopFaker {
+  
+  private final Faker faker;
+
+  public String createUrl() {
+    return faker.lorem().characters(1, 256);
+  }
+
+  public String createName() {
+    return faker.lorem().characters(1, 256);
+  }
+
+  public String createAddress() {
+    return faker.lorem().characters(1, 256);
+  }
+
+  public Integer createDistance() {
+    return faker.hashCode();
+  }
+
+  public Integer createMinutes() {
+    return faker.hashCode();
+  }
+}
